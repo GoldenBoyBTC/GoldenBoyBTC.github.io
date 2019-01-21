a8b3659b982e391996166058184c94a7be869745
GoldenBoi <34171228+GoldenBoyBTC@users.noreply.github.com>
2019-01-21 20:26:29 +0200

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,3 +1,3 @@
 var s=["█","▓","▒","░","█","▓","▒","░","█","▓","▒","░","<",">","/"];
-//baffle('#menutitle', { characters: s }).start().text(currentText => 'samy kamkar').reveal(5000, 1000);
-baffle('#menutitle', { characters: s }).start().text(function (currentText) { return 'samy kamkar'; }).reveal(5000, 1000);
+//baffle('#menutitle', { characters: s }).start().text(currentText => 'pavlos baritakis').reveal(5000, 1000);
+baffle('#menutitle', { characters: s }).start().text(function (currentText) { return 'pavlos baritakis'; }).reveal(5000, 1000);
